0f2a8a20d43e0ce63a45bb499b96003446501267
Lowell Kirsh <lowellk@gmail.com>
2012-06-10 11:55:10 -0700

    remove dead code

--- bookmarklet.js
+++ bookmarklet.js
@@ -27,29 +27,6 @@
     jquery: '//ajax.microsoft.com/ajax/jquery/jquery-1.7.2.min.js'
   };
 
-  // http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
-  function loadScript(url, callback) {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-
-    if (script.readyState) {  //IE
-      script.onreadystatechange = function () {
-        if (script.readyState == "loaded" ||
-          script.readyState == "complete") {
-          script.onreadystatechange = null;
-          callback();
-        }
-      };
-    } else {  //Others
-      script.onload = function () {
-        callback();
-      };
-    }
-
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
-  }
-
   // taken from https://raw.github.com/getify/LABjs/master/LAB.min.js
   function loadLabJs() {
     /*! LAB.js (LABjs :: Loading And Blocking JavaScript)
